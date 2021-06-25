c81a9f48ad756af0578d05b57486c6a2b4acc6c3
Sarah Marshall <sr70079@gmail.com>
2021-06-25 09:57:57 -0400

    navbar fix

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,26 +5,30 @@ import { Link } from "react-router-dom";
 function NavBar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="/">Sarah Marshall</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">Home </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/portfolio">Portfolio </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/resume">Resume </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact</Link>
-          </li>
-      </ul>
-    </div>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Sarah Marshall</a>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Home </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/portfolio">Portfolio </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/resume">Resume </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </li>
+          </ul>
+        </div>
+      </div>
   </nav>
   );
 }

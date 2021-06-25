3d99e09149fd1f8086f9e0e4313deacd62484390
Sarah Marshall <sr70079@gmail.com>
2021-06-24 22:39:35 -0400

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router , Route } from "react-router-dom";
+import { HashRouter as Router , Route } from "react-router-dom";
 import NavBar from "./components/Nav/Nav";
 import Home from "./pages/Home";
 import Projects from "./pages/Portfolio";

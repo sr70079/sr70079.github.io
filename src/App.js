23de67f0775b946d53bc3f378cc1e937a4563e75
Sarah Marshall <sr70079@gmail.com>
2021-06-24 23:51:33 -0400

    fixed some configuration issues with hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {HashRouter, Route } from "react-router-dom";
 import NavBar from "./components/Nav/Nav";
 import Home from "./pages/Home";
 import Projects from "./pages/Portfolio";
@@ -7,24 +7,17 @@ import Resume from "./pages/Resume"
 import Contact from "./pages/Contact";
 import Footer from "./components/StickyFooter/StickyFooter";
 
-
-
 function App() {
-
   return (
     <div>
-
-        <Switch>
+        <HashRouter>
           <NavBar />
-          {/* <Router> */}
-            <Route exact path = "/" component = {Home}/>
-            <Route path = "/portfolio" component = {Projects}/>
-            <Route path = "/resume" component = {Resume}/>
-            <Route path = "/contact" component = {Contact}/>
-          {/* </Router> */}
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/portfolio" component={Projects}/>
+          <Route exact path="/resume" component={Resume}/>
+          <Route exact path="/contact" component={Contact}/>
           <Footer />
-        </Switch>
-
+        </HashRouter>
     </div>
   );
 }
